bb494441194eb2eba07c156cd4b6f7243789e43a
q210164 <v.dequecker@student.helmo.be>
2024-11-05 13:43:58 +0100

    Page profil part.2

--- src/pages/ProfilPage.js
+++ src/pages/ProfilPage.js
@@ -3,10 +3,12 @@ import { useParams, useNavigate } from 'react-router-dom';
 import GoalsPage from './Goals';
 import height from '../assets/images/height.png';
 import weight from '../assets/images/dumbbell.png';
+import sablier from '../assets/images/sablier.png';
 
 const ProfilPage = () => {
     const { userId } = useParams();
     const [user, setUser] = useState(null);
+    const [profilePicture, setProfilePicture] = useState('');
     const [followersCount, setFollowersCount] = useState(0);
     const [followingCount, setFollowingCount] = useState(0);
     const [error, setError] = useState(null);
@@ -21,6 +23,7 @@ const ProfilPage = () => {
                 }
                 const data = await response.json();
                 setUser(data);
+                setProfilePicture(require(`../assets/images/${data.profilePicture}`));
             } catch (error) {
                 console.error("Error fetching user profile", error);
                 setError("Could not fetch user profile.");
@@ -37,7 +40,7 @@ const ProfilPage = () => {
                 setFollowersCount(data); // Assurez-vous que le format de la réponse est correct
             } catch (error) {
                 console.error("Error fetching followers count", error);
-                setError("Could not fetch followers count.");
+                //setError("Could not fetch followers count.");
             }
         };
 
@@ -51,7 +54,7 @@ const ProfilPage = () => {
                 setFollowingCount(data); // Assurez-vous que le format de la réponse est correct
             } catch (error) {
                 console.error("Error fetching following count", error);
-                setError("Could not fetch following count.");
+                //setError("Could not fetch following count.");
             }
         };
 
@@ -72,10 +75,21 @@ const ProfilPage = () => {
             >
                 &#8592; {/* Flèche gauche */}
             </button>
-            <div className="flex items-center justify-between mb-6">
-                <span className="text-lg">{followersCount} Followers</span>
-                <h1 className="text-2xl font-bold">{user.firstName} {user.lastName}</h1>
-                <span className="text-lg">{followingCount} Follows</span>
+            <div className="flex flex-col items-center mb-6">
+                <div className="w-24 h-24 rounded-full overflow-hidden mb-4">
+                    <img
+                        src={profilePicture}
+                        alt="Photo de profil"
+                        className="object-cover w-full h-full"
+                    />
+                </div>
+                <h1 className="text-2xl font-bold text-center mb-2">
+                    {user.firstName} {user.lastName}
+                </h1>
+                <div className="flex items-center justify-between w-full mt-2">
+                    <span className="text-lg">{followersCount} Followers</span>
+                    <span className="text-lg">{followingCount} Follows</span>
+                </div>
             </div>
             <div className="flex justify-center space-x-4 mb-4">
                 <div className="flex justify-center space-x-4 mb-4 items-center">
@@ -96,11 +110,24 @@ const ProfilPage = () => {
                         <p className="text-center text-sm">Weight</p>
                     </div>
                 </div>
+                <div className="flex justify-center space-x-4 mb-4 items-center">
+                    <div className="flex flex-col items-center bg-amber-300 p-2 rounded">
+                        <img src={sablier} alt="Poids icon" className="w-10 h-10"/>
+                    </div>
+                    <div className="text-center">
+                        <span className="font-semibold">{user.age} Year</span>
+                        <p className="text-center text-sm">Age</p>
+                    </div>
+                </div>
             </div>
             <hr className="border-white mb-4"/>
             <div className="flex flex-col justify-center items-center mb-4">
                 <h2 className="font-bold text-2xl">Main goal</h2>
                 <span className="italic">{user.mainGoal}</span>
+            </div>
+            <div className="flex flex-col justify-center items-center mb-4">
+                <h2 className="font-bold text-2xl">Personnal goals</h2>
+                <span className="italic">{user.goalWeight} KG</span>
             </div>
             <hr className="border-white mb-4"/>
             <div className="flex flex-col space-y-4">
