3575c599465181d41327b2134b4df04774f73970
RubenYildiz21 <yildiz-ruben@hotmail.com>
2024-11-23 19:43:32 +0100

    modification format seance

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -3,30 +3,8 @@ import apiRequest from './api'; // Assurez-vous que './api' pointe vers le bon f
 
 export const createWorkoutSession = async (sessionData) => {
   try {
-    const user = JSON.parse(sessionStorage.getItem('user')); // Utiliser la clé string correcte
-    if (!user) throw new Error('Utilisateur non authentifié.');
 
-    const formattedData = {
-      dateSeance: new Date(sessionData.date).toISOString(),
-      user: {
-        id: user.id
-      },
-      nom: sessionData.name,
-      blocs: sessionData.exercises.map(ex => ({
-        exercice: { 
-          idExercice: ex.idExercice // Assurez-vous que 'idExercice' est le bon champ
-        },
-        reps: ex.sets.reduce((acc, set) => acc + set.reps, 0), // Total des répétitions
-        serie: ex.sets.length,
-        poids: ex.sets.reduce((acc, set) => acc + set.weight, 0) / ex.sets.length, // Moyenne des poids
-        tempsRepos: ex.tempsRepos, // Temps de repos dynamique
-        tempsDeRepetition: ex.tempsDeRepetition // Temps de répétition dynamique
-      }))
-    };
-
-    console.log('Données envoyées au serveur:', formattedData);
-    
-    const response = await apiRequest('/seances', 'POST', formattedData);
+    const response = await apiRequest('/seances', 'POST', sessionData);
 
     return response;
   } catch (error) {
